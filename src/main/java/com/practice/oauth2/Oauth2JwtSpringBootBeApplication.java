4fa480a7437cb75c9a12027ec280bce24add057f
zseilx <zseil@naver.com>
2023-08-06 13:57:49 +0900

    initial commit

---
+++ src/main/java/com/practice/oauth2/Oauth2JwtSpringBootBeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtSpringBootBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JwtSpringBootBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/oauth2/Oauth2JwtSpringBootBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JwtSpringBootBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
